3fa54a65f436dc106986707a4a35616161c6687f
AliAshraf7868 <61000982+AliAshraf7868@users.noreply.github.com>
2020-02-14 13:02:34 +0500

    Add files via upload

---
+++ simple sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"take first number for adition";
+	cin>> a ;
+	cout<<"take second number for adition";
+	cin>> b ;
+	c=a+b;
+	cout<<c;
+}
